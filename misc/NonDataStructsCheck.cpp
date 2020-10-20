57abacac8198ee941d88e560876520ea88473928
Oleksandr Koval <oleksandr.koval.dev@gmail.com>
2020-10-20 22:50:46 +0300

    Remove hand-written matchers, report bad m ember name

--- misc/NonDataStructsCheck.cpp
+++ misc/NonDataStructsCheck.cpp
@@ -15,36 +15,36 @@ using namespace clang::ast_matchers;
 namespace clang {
 namespace tidy {
 namespace misc {
-
-namespace {
-// these are from misc-non-private-member-variables-in-classes
-AST_MATCHER(CXXRecordDecl, hasMethods) {
-  // doesn't count template methods until their instantiation
-  return std::distance(Node.method_begin(), Node.method_end()) != 0;
-}
-
-AST_MATCHER(CXXRecordDecl, hasNonPublicMemberVariable) {
-  return cxxRecordDecl(has(fieldDecl(unless(isPublic()))))
-      .matches(Node, Finder, Builder);
-}
-} // namespace
-
 void NonDataStructsCheck::registerMatchers(MatchFinder *Finder) {
   Finder->addMatcher(
-      cxxRecordDecl(isStruct(),
-                    anyOf(hasMethods(), hasNonPublicMemberVariable()))
+      cxxRecordDecl(
+          isStruct(),
+          anyOf(has(cxxMethodDecl(
+                        unless(anyOf(isImplicit(), isDefaulted(), isDeleted(),
+                                     isStaticStorageClass())))
+                        .bind("method")),
+                has(fieldDecl(unless(isPublic())).bind("field"))))
           .bind("record"),
       this);
 }
 
 void NonDataStructsCheck::check(const MatchFinder::MatchResult &Result) {
 
-  const auto *MatchedRecord = Result.Nodes.getNodeAs<CXXRecordDecl>("record");
+  const auto MatchedRecord = Result.Nodes.getNodeAs<CXXRecordDecl>("record");
   assert(MatchedRecord);
-  
-  diag(MatchedRecord->getLocation(),
-       "struct %0 has methods or non-public data members")
-      << MatchedRecord;
+
+  if (const auto MatchedMethod =
+          Result.Nodes.getNodeAs<CXXMethodDecl>("method")) {
+    diag(MatchedRecord->getLocation(), "struct %0 has non-static method %1")
+        << MatchedRecord << MatchedMethod;
+  } else {
+    const auto MatchedField = Result.Nodes.getNodeAs<FieldDecl>("field");
+    assert(MatchedField);
+
+    diag(MatchedRecord->getLocation(),
+         "struct %0 has non-public data member %1")
+        << MatchedRecord << MatchedField;
+  }
 }
 
 } // namespace misc
